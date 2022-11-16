56e11964d9324e688d6b90748d340a7a89d97f6d
Lee <leewhiz119@gmail.com>
2022-11-16 08:31:39 -0500

    added checkForColumnOfFour

--- src/App.js
+++ src/App.js
@@ -29,6 +29,16 @@ const App = () => {
     }
   }
 
+  const checkForColumnOfFour = () => {
+    for (let i=0; i < 47; i++) {
+      const columnOfFour = [i, i +width, i + width *2, i + width * 3]
+      const decidedColor = currentColorArrangement[i]
+
+      if ( columnOfFour.every(square => currentColorArrangement[square] === decidedColor)) {
+        columnOfFour.forEach(square => currentColorArrangement[square] = '')
+      }
+    }
+  }
 
 //create array of random colors
   const createBoard = () => {
@@ -46,11 +56,12 @@ const App = () => {
 
   useEffect(() => {
     const timer = setInterval(() => {
+      checkForColumnOfFour()
       checkForColumnOfThree()
       setCurrentColorArrangement([...currentColorArrangement])
     }, 100)
     return () => clearInterval(timer)
-  }, [checkForColumnOfThree, currentColorArrangement])
+  }, [checkForColumnOfThree, checkForColumnOfFour, currentColorArrangement])
 
   console.log(currentColorArrangement)
 
